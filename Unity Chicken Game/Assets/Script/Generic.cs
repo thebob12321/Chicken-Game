3839cff8be3fd630c1957cbea85751c774a61bc2
Unknown <darrensmith12@live.com>
2017-11-06 20:09:10 -0700

    Update

--- Unity Chicken Game/Assets/Script/Generic.cs
+++ Unity Chicken Game/Assets/Script/Generic.cs
@@ -8,22 +8,41 @@ using UnityEngine;
 public class Generic : MonoBehaviour {
 
 	// position
-	public float posX; 
-	public float posY; 
-	public float posZ; 
-	
+	// public float posX; 
+	// public float posY; 
+	// public float posZ; 
+	public Vector3 pos;
+
+	// Rotate
+	// public float rotX;
+	// public float rotY;
+	// public float rotZ;
+	// public float rotW;
+	public Quaternion rot;
+
+	//Scale
+	// public float X = 1f;
+	// public float Y = 1f;
+	// public float Z = 1f;
+	public Vector3 scale;
 
 
 
 
 	// Use this for initialization
 	void Start () {
-		
+		//Game needs to start to work
+		scale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(posX,posY,posZ);
-		
+		// transform.position = new Vector3(posX,posY,posZ);
+		// transform.rotation = new Quaternion(rotX,rotY,rotZ,rotW);
+		// transform.localScale = new Vector3(X,Y,Z);
+
+		transform.position = pos;
+		transform.rotation = rot;
+		scale = Vector3.one;
 	}
 }
