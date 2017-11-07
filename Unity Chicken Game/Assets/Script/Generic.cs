9cbcea74e2b0c39d408cae180bed1037b3d15d2a
Unknown <darrensmith12@live.com>
2017-11-06 19:52:19 -0700

    Update

---
+++ Unity Chicken Game/Assets/Script/Generic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Generic : MonoBehaviour {
+
+	// position
+	public float posX; 
+	public float posY; 
+	public float posZ; 
+	
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(posX,posY,posZ);
+		
+	}
+}
