a0d416d87e1f32375923c7e40d9762918d2c5763
Unknown <darrensmith12@live.com>
2017-11-08 20:07:00 -0700

    Wonder

---
+++ Unity Chicken Game/Assets/Script/Wonder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wonder : MonoBehaviour {
+
+	public float moveSpeed;
+
+	void MoveForward(){
+		transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+	}
+
+	void Turn(){
+		int randomNum = Random.Range(0,360);
+		transform.Rotate(0,randomNum,0);
+	}
+
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.tag == "CheckPoint"){
+			Turn();
+		}
+		else{
+			MoveForward();
+		}
+	}
+
+}
