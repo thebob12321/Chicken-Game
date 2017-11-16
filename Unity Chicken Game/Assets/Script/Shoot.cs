bd2d386cda89ba90b337d1aa0243706426d283da
Unknown <darrensmith12@live.com>
2017-11-15 20:10:16 -0700

    Update

---
+++ Unity Chicken Game/Assets/Script/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+	public Rigidbody projectile;
+	public Transform SpawnPoint;
+	public int shootSpeed;
+	
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Rigidbody clone;
+			clone = (Rigidbody)Instantiate(projectile, SpawnPoint.position,projectile.rotation);
+			clone.velocity = SpawnPoint.TransformDirection (Vector3.forward*shootSpeed);
+		}
+	}
+}
---
+++ Unity Chicken Game/Assets/Script/wolfHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wolfHealth : MonoBehaviour {
+	public int currentHealth;
+	public int maxHealth = 3;
+	public Transform spawnPoint;
+	public int points;
+	public void TakeDamage(int amount){
+		currentHealth -= amount;
+		if (currentHealth <= 0){
+			currentHealth=0;
+			print("Wolf is Dead!");
+			scoreManager.Addpoints(points);
+			transform.position = spawnPoint.position;
+			transform.rotation = spawnPoint.rotation;
+			currentHealth = maxHealth;
+		}
+
+	}
+
+}
